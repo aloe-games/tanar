2c8b947b4a60ba090dbee080af0ce0f880f8948b
Marek Michalik <marek.michalik@interia.pl>
2015-02-14 13:36:12 +0100

    remember player name in cookie

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -13,7 +13,9 @@ var connect = function() {
         console.log("Recieve: " + message);
         receive(JSON.parse(message));
     };
-    send({command: 'join', username: prompt("Username:", "Marek")});
+    var username = prompt("Username:", getCookie("username"));
+    setCookie("username", username, 365);
+    send({command: 'join', username: username});
 };
 
 var send = function (data) {
@@ -335,4 +337,22 @@ function escapeRegExp(string) {
 
 function replaceAll(find, replace, string) {
     return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+} 
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
 }
