0998a3432f96d03a893ab34851d4eb321a7dbb88
Marek Michalik <marekmichalik43@gmail.com>
2024-11-01 12:24:02 +0100

    Add support for websockets

--- web.py
+++ web.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask_sock import Sock
 
 app = Flask(__name__)
+sock = Sock(app)
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@sock.route('/server')
+def echo(ws):
+    while True:
+        data = ws.receive()
+        print(data)
+        ws.send(data)
